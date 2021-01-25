888263f5b117c4d121c2a519ca28c924a3b757e5
khadija-ibrahim <77287877+khadija-ibrahim@users.noreply.github.com>
2021-01-25 07:02:21 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,73 @@
+//TASK 1
+let a = 3;
+let b = 5;
+let c;
+
+
+let output = '';
+output += 'let a = 3;';
+output += '\n';
+output += 'let b = 5;';
+output += '\n';
+output += 'let c;';
+output += '\n';
+output += '--------------------';
+output += '\n';
+output += 'a + b = ' + (a + b);
+output += '\n';
+output += 'a - b = ' + (a - b);
+output += '\n';
+output += 'a * b = ' + (a * b);
+output += '\n';
+output += 'a / b = ' + (a / b);
+output += '\n';
+output += 'a % b = ' + (a % b);
+output += '\n';
+output += '(a += b) : ' + (a += b);
+output += '\n';
+output += '(a -= b) : ' + (a -= b);
+output += '\n';
+output += '(a *= b) : ' + (a *= b);
+output += '\n';
+output += '(a /= b) : ' + (a /= b);
+output += '\n';
+output += '(a %= b) : ' + (a %= b);
+output += '\n';
+output += '(a == b) : ' + (a == b);
+output += '\n';
+output += '(a != b) : ' + (a != b);
+output += '\n';
+output += '(a > b) : ' + (a > b);
+output += '\n';
+output += '(a < b) : ' + (a < b);
+output += '\n';
+output += '(!a && !c) : ' + (!a && !c);
+output += '\n';
+output += '(!a || !c) : ' + (!a || !c);
+
+alert(output);
+
+//TASK 2
+
+let first_name = 'Khadija';
+let last_name = 'Ibrahim';
+let email = 'ibra0247@algonquinlive.com';
+
+let output_1 = '';
+
+output_1 += '\n';
+output_1 += 'My name is' + ' ' + first_name + ' ' + last_name + '.' + ' ' + 'You can contact me at' + ' ' + email + '.';
+
+
+alert(output_1);
+
+
+
+
+
+
+
+
+
+
+
